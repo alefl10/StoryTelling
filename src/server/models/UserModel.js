c878df0383dac6b7090ef7546cddd1651d758724
Alejandro Ferrero <alejandroferrero@outlook.com>
2019-02-12 00:01:23 -0500

    Added User Model

---
+++ src/server/models/UserModel.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+	googleID: {
+		type: String,
+		unique: true,
+		required: true,
+	},
+	firstName: {
+		type: String,
+	},
+	lastName: {
+		type: String,
+	},
+	email: {
+		type: String,
+		unique: true,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	image: {
+		type: String,
+	},
+	date: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+export default mongoose.model('users', UserSchema);
